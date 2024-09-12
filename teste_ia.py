b0bb8898f2f248b1352f6b8dae5fb8407dc0ac90
lucasalc25 <lucas.alc25@gmail.com>
2024-09-12 11:07:29 -0300

    0.1

--- teste_ia.py
+++ teste_ia.py
@@ -2,9 +2,9 @@ import time
 import pytesseract
 import pyautogui
 import cv2
+import re
 from PIL import Image
 import numpy as np
-import pickle
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -26,14 +26,15 @@ servico = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=servico, options=chrome_options)
 
 def capturar_tela(chave, x, y, xfinal, yfinal):
-     # Captura a tela e salva como imagem
+    # Aumenta o zoom em 50%
+    driver.execute_script(f"document.body.style.zoom='{1.50}'")
+
+    # Clica para mostrar o histórico
+    pyautogui.click(1826, 333)
+
+    # Captura a tela e salva como imagem
     screenshot_path = "screenshot.png"
     driver.save_screenshot(screenshot_path)
-
-    # Use o Tesseract para extrair texto da imagem
-    # Certifique-se de que o Tesseract esteja instalado e configurado no PATH do sistema
-    # Para Windows, configure o caminho abaixo se necessário:
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
     # Abre a imagem completa
     image = Image.open(screenshot_path)
@@ -44,29 +45,42 @@ def capturar_tela(chave, x, y, xfinal, yfinal):
     if chave == 1:
         image.save("screenshot_numeros.png")
         # Carregar a imagem da tela capturada
-        image = cv2.imread('screenshot_numeros.png')
+        return cv2.imread('screenshot_numeros.png')
     if chave == 2:
         image.save("screenshot_novo_numero.png")
         # Carregar a imagem da tela capturada
-        image = cv2.imread('screenshot_novo_numero.png')
+        return cv2.imread('screenshot_novo_numero.png')
     if chave == 3:
         image.save("screenshot_saldo.png")
         # Carregar a imagem da tela capturada
-        image = cv2.imread('screenshot_saldo.png')
+        return cv2.imread('screenshot_saldo.png')
     if chave == 4:
         image.save("screenshot_botao.png")
         # Carregar a imagem da tela capturada
-        image = cv2.imread('screenshot_botao.png')
+        return cv2.imread('screenshot_botao.png')
     if chave == 5:
         image.save("screenshot_win.png")
         # Carregar a imagem da tela capturada
-        image = cv2.imread('screenshot_win.png')
+        return cv2.imread('screenshot_win.png')
 
-    return image
+def tratar_imagem(imagem):
+    # Convert para escala de cinza
+    gray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-def imagem_pra_array(image):
+    # Aumenta o contraste
+    gray = cv2.convertScaleAbs(gray, alpha=2.5, beta=0)
+
+    # Aplicar um filtro de nitidez para melhorar a definição dos números
+    kernel = np.array([[0, -1, 0],
+                    [-1,  5,-1],
+                    [0, -1, 0]])
+    sharp = cv2.filter2D(gray, -1, kernel)
+
+    return sharp
+
+def imagem_pra_array(imagem):
     """Converte uma imagem PIL para um array numpy."""
-    return np.array(image)
+    return np.array(imagem)
 
 def comparar_imagens(img1, img2):
     """Compara duas imagens e retorna True se forem diferentes."""
@@ -89,19 +103,9 @@ def corrigir_numero(item):
 
 # Função para capturar os números
 def capturar_numeros():
-    image = capturar_tela(1, 590, 240, 1760, 325)
+    imagem = capturar_tela(1, 590, 240, 1790, 325)
 
-    # Convert para escala de cinza
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Aumenta o contraste
-    gray = cv2.convertScaleAbs(gray, alpha=2.5, beta=0)
-
-    # Aplicar um filtro de nitidez para melhorar a definição dos números
-    kernel = np.array([[0, -1, 0],
-                    [-1,  5,-1],
-                    [0, -1, 0]])
-    sharp = cv2.filter2D(gray, -1, kernel)
+    sharp = tratar_imagem(imagem)
 
     # Configuração personalizada do Tesseract
     custom_config = r'--oem 3 --psm 6 outputbase digits'
@@ -110,38 +114,34 @@ def capturar_numeros():
     text = pytesseract.image_to_string(sharp, config=custom_config)
 
     try:
-        # Pós-processamento
-        numeros = []
-        for item in text.split():
-            # Remove 'x' e '%' e verifica o formato esperado
-            item = item.replace('x', '').replace('%', '').replace('-', '')
-            if item.replace('.', '', 1).isdigit():
-                # Corrige números que podem estar incorretos
-                if len(item.split('.')[0]) > 2:  # Verifica se o número parece grande demais
-                    item = item[:-2] + '.' + item[-2:]
-                    item = corrigir_numero(item)
-                numeros.append(float(item))
+        # Usa expressão regular para encontrar números com mais de 2 dígitos consecutivos
+        # Separa a string em números com dois dígitos após o ponto decimal
+        numeros = re.findall(r'\d{1,3}(?:\.\d{1,2})?', text)
 
-        print("Números extraídos:", numeros)
-        return numeros
+        # Converte os números encontrados em ponto flutuante
+        numeros_convertidos = []
+
+        for numero in numeros:
+            if '.' in numero:
+                partes = numero.split('.')
+                if len(partes[-1]) == 1:
+                    numero = '.'.join(partes[:-1]) + '.' + partes[-1] + '0'  # Adiciona '0' se faltar um dígito
+                elif len(partes[-1]) > 2:
+                    numero = '.'.join(partes[:-1]) + '.' + partes[-1][:2]  # Mantém apenas 2 dígitos após o ponto
+            else:
+                numero += '.00'  # Adiciona '.00' se o número não tiver ponto decimal
+            numeros_convertidos.append(float(numero))
+        
+        print("Números tratados: ",numeros_convertidos)
+        return numeros_convertidos
      
     except ValueError as e:
         print("Erro ao converter string para float:", e)
 
 def capturar_novo_numero():
-    image = capturar_tela(2, 600, 195, 665, 225)
+    imagem = capturar_tela(2, 600, 195, 665, 225)
 
-    # Convert para escala de cinza
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Aumenta o contraste
-    gray = cv2.convertScaleAbs(gray, alpha=2.5, beta=0)
-
-    # Aplicar um filtro de nitidez para melhorar a definição dos números
-    kernel = np.array([[0, -1, 0],
-                    [-1,  5,-1],
-                    [0, -1, 0]])
-    sharp = cv2.filter2D(gray, -1, kernel)
+    sharp = tratar_imagem(imagem)
 
     # Configuração personalizada do Tesseract
     custom_config = r'--oem 3 --psm 6 outputbase digits'
@@ -185,6 +185,9 @@ def nova_lista(lista_numeros):
 
 # Função para preparar os dados
 def preparar_dados(lista_numeros):
+    if not lista_numeros:
+        raise ValueError("A lista de números está vazia.")
+    
     # Normaliza os números
     scaler = MinMaxScaler()
     numbers_normalized = scaler.fit_transform(np.array(lista_numeros).reshape(-1, 1))
@@ -207,6 +210,9 @@ def criar_modelo(input_shape):
 
 # Função para treinar a IA
 def treinar_modelo(lista_numeros):
+    if len(lista_numeros) < 2:
+        raise ValueError("A lista de números deve ter pelo menos 2 elementos para o treinamento.")
+
     X, y = preparar_dados(lista_numeros)
     model = criar_modelo(X.shape[1])
     model.fit(X, y, epochs=10, batch_size=1, verbose=1)
@@ -220,49 +226,53 @@ def fazer_previsao(model, last_number):
     prediction = scaler.inverse_transform(prediction_normalized)
     return prediction[0][0]
 
-def main():
-    # Acessa a página desejada
-    url = "https://www.claro.bet/slots/all/240/spribe/37591-806666-aviator?mode=fun"
-    driver.get(url)
 
-    # Aumenta o zoom em 50%
-    zoom_level = 1.50   
-    driver.execute_script(f"document.body.style.zoom='{zoom_level}'")
+# Acessa a página desejada
+url = "https://www.claro.bet/slots/all/240/spribe/37591-806666-aviator?mode=fun"
+driver.get(url)
 
-    # Aguarde a página carregar completamente
-    time.sleep(10)
+# Aguarde a página carregar completamente
+time.sleep(10)
 
-    # Clica para mostrar o histórico
-    pyautogui.click(1826, 333)
+lista_numeros = capturar_numeros()  # Captura os números
 
-    lista_numeros = capturar_numeros()  # Captura os números
+# Verificação da lista capturada
+if not lista_numeros:
+    print("Lista inicial não foi capturada.")
+    driver.quit()
+
+while True:
+    if lista_numeros != nova_lista(lista_numeros):
+        novo_numero = capturar_novo_numero()
+
+        lista_numeros = atualizar_lista(lista_numeros, novo_numero)
     
-    # Clica para ocultar o histórico
-    pyautogui.click(1826, 333)
+        # Treina a IA
+        modelo = treinar_modelo(lista_numeros)
 
-    # Treina a IA
-    modelo = treinar_modelo(lista_numeros)
-
-    while True:
-        novo_numero = lista_numeros[0]
         previsao = fazer_previsao(modelo, novo_numero)
         print(f"Previsão: {previsao}")
-        
-        if previsao >= 1.25:
-            resposta = input("O palpite está correto? (s/n): ")
-            if resposta.lower() == 's':
-                continue
-            else:
-                for _ in range(3):
-                    previsao = fazer_previsao(modelo, novo_numero + 1)
-                    print(f"Tentativa adicional: {previsao}")
-                    if previsao >= 1.25:
-                        break
-                else:
-                    print("Você bateu o stop.")
-                    break
-        else:
-            continue
 
-if __name__ == "__main__":
-    main()
+        if previsao >= 1.25:
+            # Clica para ocultar o histórico
+            pyautogui.click(1826, 333)
+
+            driver.execute_script(f"document.body.style.zoom='{1.00}'")
+
+            if lista_numeros != nova_lista(lista_numeros):
+
+                resposta = input("O palpite estava correto? (s/n): ")
+                
+                if resposta.lower() == 's':
+                    continue
+                else:
+                    for _ in range(3):
+                        previsao = fazer_previsao(modelo, novo_numero + 1)
+                        print(f"Tentativa adicional: {previsao}")
+                        if previsao >= 1.25:
+                            break
+                    else:
+                        print("Você bateu o stop.")
+                        break
+            else:
+                continue
